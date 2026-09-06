3e762696e72c1f416cc30664dce4399feaa972c3
Shubham <ssmunde26@gmail.com>
2026-09-06 18:38:15 +0530

    Set up Vite React project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Hospitality Resource Exchange</h1>
+      <p>Minimal Vite + React application setup (Step 1A).</p>
+    </div>
+  );
+}
+
+export default App;
