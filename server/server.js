fb8fceb7cb5eb7c55f20c5dd24ebe8bb5535e7b0
Shubham <ssmunde26@gmail.com>
2026-09-09 00:43:47 +0530

    Add request POST API

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const resources = require("./data/resources");
 
+const requests = [];
+
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/health", (req, res) => {
     res.json({
@@ -15,6 +18,12 @@ app.get("/api/health", (req, res) => {
 
 app.get("/api/resources", (req, res) => {
     res.json(resources);
+});
+
+app.post("/api/requests", (req, res) => {
+    const request = req.body;
+    requests.push(request);
+    res.status(201).json(request);
 });
 
 app.listen(5000, () => {
